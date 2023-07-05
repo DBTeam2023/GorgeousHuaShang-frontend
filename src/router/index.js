2230ac884f228a3f3eb3c34aea6ce04d9e5b07e0
うさぎ <momoyama.sawa@gmail.com>
2023-07-05 13:40:18 +0800

    fix: New store management and store selection

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import{createRouter, createWebHistory} from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import HomePageView from "@/views/HomePageView.vue";
+import ShopSelectView from "@/views/ShopSelectView.vue";
+import ShopManageView from "@/views/ShopManageView.vue";
 // import store from "@/store";
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/homepage/',
-    name: "Homepage",
-    component: HomePageView
-  },
-  {
-    path: '/login/',
-    name: 'Login',
-    component: LoginView
-  },
-  {
-    path: '/register/',
-    name: 'Register',
-    component: RegisterView
-  }
-]
+const routes =
+    [
+      {path : '/', name : 'home', component : HomeView},
+      {path : '/homepage/', name : "Homepage", component : HomePageView},
+      {path : '/login/', name : 'Login', component : LoginView},
+      {path : '/register/', name : 'Register', component : RegisterView},
+      {path : '/shopselect/', name : 'ShopSelect', component : ShopSelectView},
+      {path : '/shopmanage/', name : 'ShopManage', component : ShopManageView},
+    ]
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+    const router = createRouter({history : createWebHistory(), routes})
 
-// router.beforeEach(async (to, from, next) => {
-//   console.log(to.path);
-//   from;
-//   next();
-//
-//   const {isLogin} = await store.dispatch("getUserInfo");
-//   console.log(isLogin);
-// })
+    // router.beforeEach(async (to, from, next) => {
+    //   console.log(to.path);
+    //   from;
+    //   next();
+    //
+    //   const {isLogin} = await store.dispatch("getUserInfo");
+    //   console.log(isLogin);
+    // })
 
-export default router
-
+    export default router
