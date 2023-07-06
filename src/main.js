2fd9277c562072fd9ed01a2bf44afee448e56a23
samhar78 <1849709848@qq.com>
2023-07-06 17:37:25 +0800

    更新 main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import store from './store'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import UndrawUi from 'undraw-ui'
+import 'undraw-ui/dist/style.css'
 
-createApp(App).use(router).use(ElementPlus).use(store).mount('#app')
+createApp(App).use(router).use(ElementPlus).use(store).use(UndrawUi).mount('#app')
