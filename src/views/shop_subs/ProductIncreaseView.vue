7f83122beefde2eef831b6096be36063a5d89f2c
うさぎ <momoyama.sawa@gmail.com>
2023-07-29 15:10:15 +0800

    feat: Update Shop_subs

--- src/views/shop_subs/ProductIncreaseView.vue
+++ src/views/shop_subs/ProductIncreaseView.vue
@@ -1,90 +1,130 @@
 <template>
   <div class="add-product">
     <h1 class="title">新增商品</h1>
-    <el-form :model="form" label-width="80px">
-      <el-form-item label="商品名">
-        <el-input v-model="form.productName"></el-input>
-      </el-form-item>
-      <el-form-item label="店铺描述">
-        <el-input v-model="form.shopDescription"></el-input>
-      </el-form-item>
-      <el-form-item label="单件价格">
-        <el-input v-model.number="form.price" type="number"></el-input>
-      </el-form-item>
-      <el-form-item label="初始库存量">
-        <el-input v-model.number="form.stock" type="number"></el-input>
-      </el-form-item>
-      <el-form-item label="上传展示图片">
-        <el-upload
-          action="/upload"
-          :before-upload="beforeUpload"
-          :on-success="handleUploadSuccess"
-          :on-error="handleUploadError"
-          :file-list="fileList"
-          list-type="picture-card"
-        >
-          <i class="el-icon-plus"></i>
-        </el-upload>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm">确定</el-button>
-        <el-button @click="resetForm">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card class="card">
+          <div class="image-container" @click="openImagePicker">
+            <img class="product-image" :src="selectedImage ? selectedImage : placeholderImage" alt="Product Image" />
+          </div>
+          <el-input v-model="productName" placeholder="请输入商品名称"></el-input>
+        </el-card>
+      </el-col>
+
+      <el-col :span="12">
+        <el-card class="card">
+          <h3 class="subtitle">商品信息</h3>
+          <el-form :model="form" label-width="80px" class="form">
+            <el-form-item label="单件价格">
+              <el-input v-model.number="form.price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="初始库存">
+              <el-input v-model.number="form.stock" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品详情">
+              <el-input type="textarea" v-model="form.description"></el-input>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="24">
+        <el-card class="card">
+          <div class="image-list">
+            <ImageGallery></ImageGallery>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+    <div class="button-container">
+      <el-button type="primary" class="submit-button" @click="submitForm">确定</el-button>
+      <el-button class="reset-button" @click="resetForm">重置</el-button>
+    </div>
   </div>
+  <div></div>
 </template>
 
 <script>
-import { reactive } from 'vue';
-import { ElButton, ElForm, ElFormItem, ElInput, ElUpload } from 'element-plus';
+import { ref } from 'vue';
+import ImageGallery from './PicShow.vue';
 
 export default {
+  name: 'AddProduct',
   components: {
-    ElButton,
-    ElForm,
-    ElFormItem,
-    ElInput,
-    ElUpload,
+    ImageGallery,
   },
-  setup() {
-    const form = reactive({
-      productName: '',
-      shopDescription: '',
-      price: null,
-      stock: null,
-    });
-    // const fileList = ref([]);
-
-    // const beforeUpload = (file) => {
-    //   // 在这里可以添加图片上传前的校验逻辑
-    // };
-
-    // const handleUploadSuccess = (response, file, fileList) => {
-    //   // 图片上传成功后的处理逻辑
-    // };
-
-    // const handleUploadError = (error, file, fileList) => {
-    //   // 图片上传失败后的处理逻辑
-    // };
-
-    const submitForm = () => {
-      // 提交表单的处理逻辑
-    };
-
-    const resetForm = () => {
-      // 重置表单的处理逻辑
-    };
-
+  data() {
     return {
-      form,
-      submitForm,
-      resetForm,
+      selectedImage: '',
+      placeholderImage: 'https://via.placeholder.com/200', // Placeholder image URL
+      productName: '',
+      form: {
+        price: null,
+        stock: null,
+        description: '',
+      },
+      fileList: [], // List of uploaded files
     };
+  },
+  methods: {
+    openImagePicker() {
+        // 打开文件选择对话框（示例: 使用<input type="file">元素）
+        // 更新所选图片
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = 'image/*';
+
+        fileInput.addEventListener('change', (event) => {
+          const file = event.target.files[0];
+          if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              this.selectedImage = e.target.result;
+            };
+            reader.readAsDataURL(file);
+          }
+        });
+
+        fileInput.click();
+    },
+    beforeUpload(file) {
+      // Before uploading the file
+      // You can perform validation here if needed
+    },
+    handleUploadSuccess(response, file) {
+        // 处理成功的文件上传
+        // 在此处更新fileList
+        this.fileList.push({
+          url: response.url,
+          name: file.name,
+          // 其他属性...
+        });
+      // 其他方法...
+    },
+    handleUploadError(error, file) {
+      // Handle file upload error
+    },
+    submitForm() {
+      // Perform form submission logic
+      // You can access the form data and uploaded files here
+      console.log(this.productName);
+      console.log(this.form);
+      console.log(this.fileList);
+    },
+    resetForm() {
+    // 将表单字段重置为初始值
+    this.formData.field1 = '';
+    this.formData.field2 = '';
+    // ...
+  },
   },
 };
 </script>
 
-<style>
+<style scoped>
 .add-product {
   padding: 20px;
 }
@@ -92,5 +132,74 @@ export default {
 .title {
   font-size: 24px;
   margin-bottom: 20px;
+}
+
+.subtitle {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.card {
+  padding: 20px;
+  margin-bottom: 20px;
+  height: 270px;
+}
+
+.image-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  border: 1px dashed #ccc;
+  cursor: pointer;
+}
+
+.product-image {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+}
+
+.upload {
+  width: 104px;
+  height: 104px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  color: #999;
+  border: 1px dashed #ccc;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.upload-text {
+  margin-top: 10px;
+}
+
+.submit-button {
+  margin-top: 20px;
+}
+
+.el-row {
+  margin-left: -10px;
+  margin-right: -10px;
+}
+
+.el-col {
+  padding: 10px;
+}
+.reset-button {
+  margin-left: 10px;
+  margin-top: 20px;
+}
+.button-container {
+  display: flex;
+  justify-content: center; /* 水平居中对齐 */
+}
+
+.submit-button {
+  margin-right: 10px; /* 设置按钮间距 */
 }
 </style>
--- src/views/shop_subs/ProductManagementView.vue
+++ src/views/shop_subs/ProductManagementView.vue
@@ -3,7 +3,10 @@
     <h1 class="title">已上架商品管理</h1>
     <el-row :gutter="20" class="filters">
       <el-col :span="6">
-        <el-input v-model="searchProductName" placeholder="商品名称"></el-input>
+        <div class="search-box">
+          <el-icon><Search /></el-icon>
+          <el-input v-model="searchProductName" placeholder="商品名称"></el-input>
+        </div>
       </el-col>
       <el-col :span="6">
         <el-button type="primary" @click="searchProducts">查询</el-button>
@@ -11,91 +14,132 @@
       </el-col>
     </el-row>
     <el-row :gutter="20" class="product-list">
-      <el-col :span="6" v-for="product in filteredProducts" :key="product.id">
-        <el-card class="product-card">
-          <a href="" target="_blank">
-          <div class="product-image">
-            <img :src="product.image" alt="商品图片">
+      <el-col :span="5.5" v-for="(product, index) in currentProducts" :key="product.id"  :gutter="20">
+        <Card class="card">
+        <div :style="{ color: isHovered[index] ? '#69c0ff' : '' }">
+          <img class="product-image" :src="product.image" alt="Product Image" />
+          <h3 class="product-name">{{ product.name }}</h3>
+          <div class="button-group">
+            <el-button type="primary" size="small" @click="editProduct(index)">修改</el-button>
+            <el-button type="danger" size="small" @click="deleteProduct(index)">删除</el-button>
           </div>
-        </a>
-          <div class="product-info">
-            <h3 class="product-name">{{ product.name }}</h3>
-            <div class="product-actions">
-              <el-button type="primary" size="small" @click="editProduct(product)">修改</el-button>
-              <el-button type="danger" size="small" @click="deleteProduct(product)">删除</el-button>
-            </div>
-          </div>
-        </el-card>
+        </div>
+      </Card>
       </el-col>
     </el-row>
+    <el-pagination
+      background
+      :current-page="currentPage"
+      :page-size="pageSize"
+      :total="filteredProducts.length"
+      @current-change="handlePageChange"
+    ></el-pagination>
   </div>
 </template>
 
-<script>
-import { computed,reactive, ref } from 'vue';
-import { ElButton, ElCard, ElCol, ElInput, ElRow } from 'element-plus';
+<script setup>
+import { computed, reactive, ref } from 'vue';
+import { ElButton, ElCol, ElInput, ElRow, ElIcon, ElPagination } from 'element-plus';
+import Card from "@/components/common/Card.vue";
+import { Search } from '@element-plus/icons-vue';
 
-export default {
-  components: {
-    ElButton,
-    ElCard,
-    ElCol,
-    ElInput,
-    ElRow,
+const products = reactive([
+  {
+    id: '1',
+    name: '商品A',
+    image: 'https://picsum.photos/200/200',
   },
-  setup() {
-    const products = reactive([
-      {
-        id: '1',
-        name: '商品A',
-        image:
-          'https://picsum.photos/200/300',
-      },
-      {
-        id: '2',
-        name: '商品B',
-        image:
-          'https://picsum.photos/200/300',
-      },
-      // 其他商品数据...
-    ]);
-
-    const searchProductName = ref('');
-
-    const searchProducts = () => {
-      // 根据商品名称进行查询逻辑
-    };
-
-    const resetFilters = () => {
-      searchProductName.value = '';
-    };
-
-    const filteredProducts = computed(() => {
-      // 根据商品名称进行筛选逻辑
-      return products.filter((product) =>
-        product.name.includes(searchProductName.value)
-      );
-    });
-
-    // const editProduct = (product) => {
-    //   // 编辑商品
-    // };
-
-    // const deleteProduct = (product) => {
-    //   // 删除商品
-    // };
-
-    return {
-      searchProductName,
-      searchProducts,
-      resetFilters,
-      filteredProducts,
-    };
+  {
+    id: '2',
+    name: '商品B',
+    image: 'https://picsum.photos/200/200',
   },
+  {
+    id: '3',
+    name: '商品C',
+    image: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '4',
+    name: '商品D',
+    image: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '5',
+    name: '商品E',
+    image: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '6',
+    name: '商品F',
+    image: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '7',
+    name: '商品G',
+    image: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '8',
+    name: '商品H',
+    image: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '9',
+    name: '商品I',
+    image: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '10',
+    name: '商品J',
+    image: 'https://picsum.photos/200/200',
+  },
+  // 更多商品...
+]);
+
+const searchProductName = ref('');
+const isHovered = reactive(Array(products.length).fill(false));
+const currentPage = ref(1);
+const pageSize = 8;
+
+const searchProducts = () => {
+  // 根据商品名称进行查询逻辑
 };
+
+const resetFilters = () => {
+  searchProductName.value = '';
+  currentPage.value = 1;
+};
+
+const filteredProducts = computed(() => {
+  const searchText = searchProductName.value.trim().toLowerCase();
+  if (searchText === '') {
+    return products;
+  } else {
+    return products.filter(product => product.name.toLowerCase().includes(searchText));
+  }
+});
+
+const currentProducts = computed(() => {
+  const startIndex = (currentPage.value - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  return filteredProducts.value.slice(startIndex, endIndex);
+});
+
+const handlePageChange = (newPage) => {
+  currentPage.value = newPage;
+};
+
 </script>
 
-<style>
+<style scoped>
+
+.card{
+  width: 240px;
+  height: 325px;
+  margin-top: 20px
+}
+
 .product-management {
   padding: 20px;
 }
@@ -122,8 +166,7 @@ export default {
 }
 
 .product-image {
-  text-align: center;
-  margin-bottom: 10px;
+  border-radius: 20px;
 }
 
 .product-info {
@@ -141,5 +184,12 @@ export default {
 .product-actions {
   display: flex;
   justify-content: space-between;
+}
+.search-box {
+  display: flex;
+  align-items: center;
+}
+.search-box .el-input {
+  margin-left: 10px; /* 修改 icon 和 input 之间的间距 */
 }
 </style>
