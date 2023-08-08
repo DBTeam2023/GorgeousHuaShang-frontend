984579412460cf8998f46070c9ed7bef4bb2d4fb
LinWang1225 <534605478@qq.com>
2023-08-08 11:23:16 +0800

    加入新组件和部分逻辑，尚未完成美化

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
-import{createRouter, createWebHashHistory} from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
-import HomePageView from "@/views/HomePageView.vue";
+import HomePageView from "@/views/HomePageView/HomePageView.vue";
+import ClassificationView from "@/views/HomePageView/ClassificationView.vue";
 import ShopSelectView from "@/views/ShopSelectView.vue";
 import ShopManageView from "@/views/ShopManageView.vue";
 import ShopView from "@/views/ShopView.vue";
@@ -12,173 +13,182 @@ import OrderView from "@/views/OrderView.vue";
 import GoodsdetailView from "@/views/GoodsdetailView.vue";
 import UserInfoView from "@/views/UserInfoView.vue"
 import store from "@/store";
-import {ElMessage} from "element-plus";
+import { ElMessage } from "element-plus";
 import NotFound from "@/views/NotFound.vue";
 
 /// requestAuth 代表是否需要登录才能访问界面
 
 const routes =
-    [
-        {
-          path : '/',
-          name : 'home',
-          redirect: "/homepage/",
-          component : HomeView,
-          meta: {
-              requestAuth: false,
-          }
-        },
-        {
-          path : '/homepage/',
-          name : "Homepage",
-          component : HomePageView,
-          meta: {
-              requestAuth: false,
-          }
-        },
-        {
-          path : '/login/',
-          name : 'Login',
-          component : LoginView,
-          meta: {
-              requestAuth: false,
-          }
-        },
-        {
-          path : '/register/',
-          name : 'Register',
-          component : RegisterView,
-          meta: {
-              requestAuth: false,
-          }
-        },
-        {
-          path : '/shopselect/',
-          name : 'ShopSelect',
-          component : ShopSelectView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path : '/shopmanage/',
-          name : 'ShopManage',
-          component : ShopManageView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path : '/shop/',
-          name : 'Shop',
-            component : ShopView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path: '/cart/',
-          name: 'Cart',
-          component: CartView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path: '/goodsdetail/',
-          name: 'Goodsdetail',
-          component: GoodsdetailView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path : '/orderdetail/',
-          name : 'OrderDetail',
-          component : OrderDetailView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path : '/order/',
-          name : 'Order',
-          component : OrderView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path: '/goodsdetail/',
-          name: 'Goodsdetail',
-          component: GoodsdetailView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-          path: '/userinfo/',
-          name: 'UserInfo',
-          component: UserInfoView,
-          meta: {
-              requestAuth: true,
-          }
-        },
-        {
-            path: '/:pathMatch(.*)*',
-            name: 'NotFound',
-            component: NotFound,
-            meta: {
-                requestAuth: false,
-            }
-        },
-    ]
-
-    const router = createRouter({history : createWebHashHistory(), routes})
-
-    router.beforeEach(async (to, from, next) => {
-      console.log("当前页面url: " + to.path);
-
-      let flag = 1;
-      const token = localStorage.getItem("jwtToken");
-      let isLogin = false;
-
-      if (token) {
-        try {
-            // 如果有token且没过期能获取到user信息，则设置为当前状态为登录
-            isLogin = await store.dispatch("getUserInfoForRouter");
-            console.log("当前页面拥有的信息如下：");
-            console.log(store.state.user);
-            // isLogin = false;
-            if (isLogin === false) {
-                ElMessage({
-                    message: "请先进行登陆操作!",
-                    type: "warning",
-                });
-                router.push({ name: "Login" });
-            }
-        } catch (error) {
-            if (to.path !== "/login/" && to.path !== "/register/") {
-                ElMessage({
-                    message: "获取登录信息失败，请先进行登陆操作!",
-                    type: "warning",
-                });
-            }
-        }
-      } else {
-          flag = 2;
+  [
+    {
+      path: '/',
+      name: 'home',
+      redirect: "/homepage/",
+      component: HomeView,
+      meta: {
+        requestAuth: false,
       }
-
-      if (to.meta.requestAuth && !isLogin) {
-          if (flag === 1) {
-              next();
-          } else {
-              next({ name: "Login" });
-          }
-      } else {
-          next();
+    },
+    {
+      path: '/homepage/',
+      name: "Homepage",
+      component: HomePageView,
+      meta: {
+        requestAuth: false,
       }
+    },
+    {
+      path: '/classify/',
+      name: "Classify",
+      component: ClassificationView,
+      meta: {
+        requestAuth: false,
+      }
+    },
+    {
+      path: '/login/',
+      name: 'Login',
+      component: LoginView,
+      meta: {
+        requestAuth: false,
+      }
+    },
+    {
+      path: '/register/',
+      name: 'Register',
+      component: RegisterView,
+      meta: {
+        requestAuth: false,
+      }
+    },
+    {
+      path: '/shopselect/',
+      name: 'ShopSelect',
+      component: ShopSelectView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/shopmanage/',
+      name: 'ShopManage',
+      component: ShopManageView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/shop/',
+      name: 'Shop',
+      component: ShopView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/cart/',
+      name: 'Cart',
+      component: CartView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/goodsdetail/',
+      name: 'Goodsdetail',
+      component: GoodsdetailView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/orderdetail/',
+      name: 'OrderDetail',
+      component: OrderDetailView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/order/',
+      name: 'Order',
+      component: OrderView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/goodsdetail/',
+      name: 'Goodsdetail',
+      component: GoodsdetailView,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/userinfo/:selected?', //设置动态参数，可有可无
+      name: 'UserInfo',
+      component: UserInfoView,
+      props: true,
+      meta: {
+        requestAuth: true,
+      }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound,
+      meta: {
+        requestAuth: false,
+      }
+    },
+  ]
 
-    })
+const router = createRouter({ history: createWebHashHistory(), routes })
 
-    export default router
+router.beforeEach(async (to, from, next) => {
+  console.log("当前页面url: " + to.path);
+
+  let flag = 1;
+  const token = localStorage.getItem("jwtToken");
+  let isLogin = false;
+
+  if (token) {
+    try {
+      // 如果有token且没过期能获取到user信息，则设置为当前状态为登录
+      isLogin = await store.dispatch("getUserInfoForRouter");
+      console.log("当前页面拥有的信息如下：");
+      console.log(store.state.user);
+      // isLogin = false;
+      if (isLogin === false) {
+        ElMessage({
+          message: "请先进行登陆操作!",
+          type: "warning",
+        });
+        router.push({ name: "Login" });
+      }
+    } catch (error) {
+      if (to.path !== "/login/" && to.path !== "/register/") {
+        ElMessage({
+          message: "获取登录信息失败，请先进行登陆操作!",
+          type: "warning",
+        });
+      }
+    }
+  } else {
+    flag = 2;
+  }
+
+  if (to.meta.requestAuth && !isLogin) {
+    if (flag === 1) {
+      next();
+    } else {
+      next({ name: "Login" });
+    }
+  } else {
+    next();
+  }
+
+})
+
+export default router
---
+++ src/views/HomePageView/HomePageView.vue
@@ -0,0 +1,94 @@
+<!--首页-->
+<!--html模板-->
+<template>
+  <el-container class="container">
+
+    <!--把搜索栏、购物车固定在顶部-->
+    <div>
+      <SearchBar />
+    </div>
+    <!--分类标签-->
+    <!-- <el-tag v-for="tag in tags" :key="tag.name" class="mx-1" closable :type="tag.type">
+      {{ tag.name }}
+    </el-tag> -->
+    <!--分类和轮播图、个人信息-->
+    <div class="homeBanner">
+      <HomeBanner />
+    </div>
+
+
+
+
+    <!--排序-->
+    <div class="sort">
+      <SortRow />
+    </div>
+
+
+    <!--推荐商品-->
+    <div>
+      <RecommendCol />
+    </div>
+
+    <!--底部-->
+    <el-footer class="footer" justify="center">
+      footer
+
+    </el-footer>
+
+
+  </el-container>
+</template>
+
+  <!--js脚本函数-->
+<script setup>
+//import { ElHeader, ElMenu, ElMenuItem, ElSubMenu, ElButton, ElRow, ElCol, ElInput } from 'element-plus';
+import SearchBar from "@/components/HomePage/SearchBar.vue"
+import HomeBanner from "@/components/HomePage/HomeBanner.vue"
+import RecommendCol from "@/components/HomePage/RecommendCol.vue"
+import SortRow from "@/components/HomePage/SortRow.vue"
+// import { ref } from 'vue'
+
+// const tags = ref([
+//   { name: 'Tag 1', type: '' },
+//   { name: 'Tag 2', type: 'success' },
+//   { name: 'Tag 3', type: 'info' },
+//   { name: 'Tag 4', type: 'warning' },
+//   { name: 'Tag 5', type: 'danger' },
+// ])
+
+  //可以使用v-model指令将搜索框中的文本内容与search数据属性进行双向绑定，以便在组件中可以实时获取和更新搜索框中的内容
+</script>
+
+
+  <!--CSS风格-->
+<style scoped>
+.container {
+  background-color: #b6e0eb;
+  margin-top: 5px;
+  height: 100%;
+}
+
+.sort-select {
+  justify-content: right;
+  margin-right: 10px;
+  display: flex;
+  float: right;
+
+}
+
+.homeBanner {
+  border-radius: 10px;
+
+}
+
+.footer {
+  margin: 10px 60px;
+  /*上下左右四个方向的外部空白区域均为 20px */
+  border: 1px solid #fffcfc;
+  /* 设置边框样式 */
+  border-radius: 10px;
+  /* 设置圆角半径 */
+  background-color: #f5f5f5;
+}
+</style>
