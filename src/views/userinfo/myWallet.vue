adc9fc72d79c86752b9b0c0c1d1dd87df290db91
Luluy233 <wayi_star@163.com>
2023-08-22 10:09:11 +0800

    钱包基本逻辑功能完成

--- src/views/userinfo/myWallet.vue
+++ src/views/userinfo/myWallet.vue
@@ -24,25 +24,6 @@
 
                     <!-- 右栏 -->
                     <el-col :span="12"  style="display: flex; flex-direction: column; justify-content: center; align-items: center">
-                        <!-- 全部余额 -->
-                        <h4>全部余额：{{walletForm.balance}}</h4>
-                        <!-- 钱包充值对话框dialog -->
-                        <div>
-                            <el-button type="primary" :disabled="isButtonDisabled" @click="dialogRechargeVisible=true">余额充值</el-button>
-                            <el-dialog v-model="dialogRechargeVisible" title="充值">
-                                <el-form :model="updateForm">
-                                <el-form-item label="请输入充值金额" :label-width="formLabelWidth">
-                                    <el-input v-model="updateForm.amount" autocomplete="off" />
-                                </el-form-item>
-                                </el-form>
-                                <template #footer>
-                                    <span class="dialog-footer">
-                                        <el-button @click="dialogRechargeVisible = false">取消</el-button>
-                                        <el-button type="primary" @click="handleRecharge">确定</el-button>
-                                    </span>
-                                </template>
-                            </el-dialog>
-                        </div>
                         <!-- 钱包状态 -->
                         <h4>钱包状态：
                             <template v-if="walletForm.status === true">
@@ -52,26 +33,26 @@
                                 冻结
                             </template>
                         </h4> 
-                        <!-- 钱包状态修改select选择框 -->
+                        <!-- 全部余额 -->
+                        <h4>全部余额：{{walletForm.balance}}</h4>
+                        <!-- 钱包充值对话框dialog -->
                         <div>
-                            <el-button type="primary" @click="EditStatus">修改状态</el-button>
-                            <el-dialog v-model="dialogEditVisible" title="修改钱包状态">
-                                <el-form :model="updateForm">
-                                <el-form-item>
-                                    <el-radio-group v-model="walletForm.status" @change="handleStatusChange">
-                                        <el-radio :label='true' >活跃</el-radio>
-                                        <el-radio :label='false' >冻结</el-radio>
-                                    </el-radio-group>
-                                </el-form-item>
+                            <el-button type="primary" :disabled="isButtonDisabled" @click="dialogRechargeVisible=true">余额充值</el-button>
+                            <el-dialog v-model="dialogRechargeVisible" title="充值">
+                                <el-form :model="updateForm" :rules="RechargeRules" ref="formRef">
+                                    <el-form-item label="请输入充值金额" :label-width="formLabelWidth" prop="amount">
+                                        <el-input v-model.number="updateForm.amount" autocomplete="off" />
+                                    </el-form-item>
                                 </el-form>
                                 <template #footer>
                                     <span class="dialog-footer">
-                                        <el-button @click="handleCancel">取消</el-button>
-                                        <el-button type="primary" @click="handleEdit">确定</el-button>
+                                        <el-button @click="dialogRechargeVisible = false">取消</el-button>
+                                        <el-button type="primary" @click="handleRecharge">确定</el-button>
                                     </span>
                                 </template>
                             </el-dialog>
                         </div>
+
                     </el-col>
                 </el-row>
             </el-card>
@@ -84,24 +65,21 @@
   
 <script setup>
     import {reactive,ref} from 'vue'
-    import { computed,onMounted } from 'vue';
+    import { computed } from 'vue';
     import { ElMessage } from 'element-plus' //消息框提示
 
-    import { getWallet, updateWalletStatus, rechargeWallet,addWallet} from '@/api/mywallet'
+    import { getWallet, rechargeWallet} from '@/api/mywallet'
 
     const walletExit=ref(true);//是否开通钱包功能标志
 
     // 钱包：从后端获取的数据
     const walletForm=reactive({
-        balance:122.4,
+        balance:0,
         status:true,
     })
 
-    // walletForm的对象副本
-    const form=reactive({
-        balance:122.4,
-        status:true,
-    })
+    //创建表单引用
+    const formRef = ref(null);
 
     // 余额颜色
     const statusColors = ref({
@@ -120,39 +98,41 @@
     });
 
     
-    const dialogEditVisible = ref(false) // 状态修改对话框可见
+    // const dialogEditVisible = ref(false) // 状态修改对话框可见
     const dialogRechargeVisible = ref(false) //余额充值对话框可见
     const formLabelWidth = '140px'
 
     // 充值的金额
     const updateForm=reactive({
-        amount:0,
+        amount:null,
     });
 
-    // 为用户添加钱包
-    const handleAdd = () => {
-
-        addWallet({
-            token:"Bearer " + localStorage.getItem("jwtToken"),        
-            balance:0,
-        })
-        .then(resp=>{
-            walletForm.balance=resp.data.balance;
-            walletForm.status=resp.data.status;
-            walletExit=true;
-            ElMessage({
-                message:'开通钱包功能成功！',
-                type:'success',
-            })
-        })
-        .catch(err=>{
-            ElMessage({
-                message:'开通钱包功能失败！',
-                type:'error',
-            })
-        })
+    // 充值金额条件
+    const validateAmount = (rule,value,callback) => {
+        if(!value){
+            return callback(new Error('请输入充值金额(0~9999999.99)'));
+        }
+        else if(!Number.isInteger(value)){
+            callback(new Error('请输入数字'));
+        }
+        else if(value <= 0){
+            callback(new Error('充值金额必须大于0'));
+        }
+        else if(value + walletForm.balance > 99999999.99){
+            callback(new Error('充值后总余额不能高于99999999.99'));
+        }
+        else{
+            callback();
+        }   	
     }
 
+    // 充值规则
+    const RechargeRules = reactive({
+        amount:[{
+            validator:validateAmount,
+            trigger:'change',
+        }]
+    })
 
     // 获取用户钱包信息
     getWallet({
@@ -161,138 +141,58 @@
     .then(resp => {
         walletForm.balance=resp.data.balance;
         walletForm.status=resp.data.status;
-        form.value.balance=walletForm.balance;
-        form.value.status=walletForm.status;
     })
-    .catch(resp => {
-        // 若用户不存在钱包
-        if(resp.code === 404){
-            //新建钱包
-            console.log('用户钱包不存在，新建钱包！')
-            addWallet({
-            token:"Bearer " + localStorage.getItem("jwtToken"),        
-            balance:0,
-        })
-        }
-        else{
-            ElMessage({
+    .catch(error => {
+        console.log('获取失败！');
+        console.log(error);
+        ElMessage({
                     message: '获取钱包信息错误，请重试！',
                     type: 'error',
                 })
-        }
-
     });
-
-
-    // 钩子函数：
-    // onMounted(() => {   
-    //     getWallet({
-    //     token:"Bearer " + localStorage.getItem("jwtToken"),        
-    // });
- 
-    // })
-
 
     // 充值处理函数
     const handleRecharge = () => {
-    
-        // 判断输入金额是否正确
-        if (updateForm.amount&&(updateForm.amount+walletForm.balance < 99999999.99)) {
-            // api:充值金额recharge，重新获取walletForm的值并显示
-            rechargeWallet({
-                token:"Bearer " + localStorage.getItem("jwtToken"), 
-                amount:updateForm.amount,
-            })
-            .then(resp=>{
-                walletForm.balance=resp.data.balance;
-                walletForm.status=resp.data.status;
-                form.balance=walletForm.balance;
-                form.status=walletForm.status;
-                console.log('充值成功！');
-                            //充值成功提示
-                ElMessage({
-                    message: '充值成功！',
-                    type: 'success',
+        console.log(updateForm.amount);
+
+        formRef.value.validate((valid) => {
+            if(valid){
+                rechargeWallet({
+                    token:"Bearer " + localStorage.getItem("jwtToken"), 
+                    amount:updateForm.amount,
                 })
-            })
-            .catch(err=>{
-                console.log('充值失败！');
-                console.log(err);
-                ElMessage({
-                    message: '充值失败',
-                    type: 'error',
+                .then(resp=>{
+                    console.log(updateForm.amount);
+                    
+                    walletForm.balance=resp.data.balance;
+                    walletForm.status=resp.data.status;
+
+                    ElMessage({
+                        message: '充值成功！',
+                        type: 'success',
+                    })
+
+                    updateForm.amount = null;
+
                 })
-            })
-        }
-        else{
-            ElMessage({
-                message: '充值金额不能超过999999999.99',
-                type: 'error',
-            })
-        }
-        updateForm.amount = 0;
+                .catch(err=>{
+                    console.log('充值失败！');
+                    console.log(err);
+                    ElMessage({
+                        message: '充值失败',
+                        type: 'error',
+                    })
+                })
+            }
+            else {
+                ElMessage({
+                        message: '请检查输入金额是否符合要求',
+                        type: 'error',
+                    })
+            }
+        })
         dialogRechargeVisible.value = false;
     };
-
-
-    //钱包状态处理函数1：按钮-修改状态
-    const EditStatus = () => {
-        console.log('edit');
-        dialogEditVisible.value=true; // 对话框设置为可见
-
-        //暂存原始数据
-        form.status=walletForm.status;
-        form.balance=walletForm.balance;
-
-    }
-
-
-    //钱包状态处理函数2：按钮-保存修改状态
-    const handleEdit = () => {
-
-        //后端api：修改钱包状态
-        updateWalletStatus({
-                token:"Bearer " + localStorage.getItem("jwtToken"), 
-                status:walletForm.status,
-            })
-            .then(resp=>{
-                walletForm.balance=resp.data.balance;
-                walletForm.status=resp.data.status;
-                form.status=resp.data.status;
-                form.balance=resp.data.balance;
-
-                            //充值成功提示
-                ElMessage({
-                    message: '修改成功！',
-                    type: 'success',
-                })
-            })
-            .catch(err=>{
-                //恢复为原数据
-                walletForm.status=form.status;
-                walletForm.balance=form.balance;
-                ElMessage({
-                    message: '修改失败,请重试！',
-                    type: 'error',
-                })
-            })
-
-        dialogEditVisible.value = false; 
-    }
-
-    // 钱包状态处理函数3：按钮-取消修改状态
-    const handleCancel=()=>{
-        console.log('cancel');
-
-        // 还原为未修改前的数据
-        walletForm.status = form.status;
-        walletForm.balance = form.balance;
-
-        // 对话框不可见
-        dialogEditVisible.value = false;
-    }
-
-
 </script>
 
 <style lang="scss" scoped>
