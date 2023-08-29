c76d0d26c1e5c2133f5a63ca57bce1ee5262e748
Jiayi Fan <105704921+Fan0110@users.noreply.github.com>
2023-08-29 23:42:41 +0800

    完善登录注册界面UI

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,7 +1,7 @@
 <!--  -->
 <template>
   <div id="login">
-<!--     PARTICLE SYSTEM-->
+    <!--     PARTICLE SYSTEM-->
     <div class="page-bg bg-blur"></div>
 
     <div class="animation-wrapper">
@@ -14,66 +14,29 @@
     <!--登录框-->
     <div class="login-area">
       <div class="login-block">
-        <div class="side-block">
-          <!--走马灯-->
-          <el-carousel
-              height="100%"
-              arrow="never"
-              indicator-position="none"
-              direction="horizontal"
-              :autoplay="true"
-              :interval="5000"
-              style="width: 50%; height: 70%; margin-left: 15%"
-          >
-            <el-carousel-item
-                v-for="(photo, index) in loginImages"
-                :key="index"
-            >
-              <el-image
-                  fit="contain"
-                  style="width: 100%; height: 100%"
-                  :src="HuashangLogo"
-              >
-              </el-image>
-            </el-carousel-item>
-          </el-carousel>
+        <div class="left-block">
+          <img src="@/assets/login/LOGO.svg" class="logo-image" @click="goToHomePage">
         </div>
-        <div style="width: 30%; height: 100%">
-          <div class="horizontal-flex-box" style="margin-top: 5%">
-            <h class="small-font" style="margin-left: 2%"
-            >华裳异彩民族服饰电商平台</h>
-          </div>
-          <div class="horizontal-flex-box" style="margin-top: 5%">
-            <h class="login-font" style="margin-left: 10%">登录</h>
-          </div>
-          <div class="vertical-flex-box">
-            <form style="margin-top: 10%">
-              <input
-                  type="text"
-                  class="email"
-                  placeholder="用户名"
-                  v-model="loginForm.username"
-              />
-              <input
-                  type="password"
-                  class="pwd"
-                  placeholder="密码"
-                  v-model="loginForm.password"
-              />
-            </form>
-            <!--登录按钮-->
-
-            <div
-                class="btn effect01"
-                target="_blank"
-                style="margin-top: 20%"
-                @click="login"
-            >
-              <span style="margin-left: 40%; font-size: 1.3em">登录</span>
+        <div class="right-block">
+          <div class="box">
+            <div class="horizontal-flex-box" style="margin-top: 10%">
+              <h class="small-font">新生态民族服饰电商平台</h>
             </div>
-            <el-checkbox v-model="checkPassword" style="margin-top: 2%"
-            >记住我</el-checkbox
-            >
+            <div class="horizontal-flex-box" style="margin-top: 5%">
+              <h class="login-font">登录</h>
+            </div>
+            <div class="vertical-flex-box">
+              <form style="margin-top: 10%">
+                <input type="text" class="email" placeholder="用户名" v-model="loginForm.username" />
+                <input type="password" class="pwd" placeholder="密码" v-model="loginForm.password" />
+              </form>
+              <el-checkbox v-model="checkPassword" style="margin-top: 5%">记住我</el-checkbox>
+
+              <!--登录按钮-->
+              <div class="btn effect01" target="_blank" style="margin-top: 10%" @click="login">
+                <span style="margin-left: 40%; font-size: 1.3em">登录</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -82,17 +45,18 @@
 </template>
 
 <script setup>
-import HuashangLogo from '../../assets/login/HuashangLogo.png'
-import {doLogin, getUserInfo} from "@/api/login";
+import HuashangLogo from '../../assets/login/LOGO.svg'
+import { doLogin, getUserInfo } from "@/api/login";
 import store from "@/store";
-import {onMounted, ref} from "vue";
-import {ElMessage} from "element-plus";
+import { onMounted, ref } from "vue";
+import { ElMessage } from "element-plus";
 import router from "@/router";
 import axios from "axios";
 
-const loginImages = [
-  "./assets/login/HuashangLogo.png",
-];
+const goToHomePage = () => {
+  router.push('/homepage/');
+};
+
 let loginForm = ref({
   username: "",
   password: "",
@@ -102,53 +66,53 @@ let checkPassword = ref(true);
 
 const login = () => {
   doLogin(loginForm.value)
-      .then(resp => {
-        if (resp.msg === 'success') {
-          ElMessage({
-            message: '登录成功',
-            type: 'success',
-          })
-          localStorage.setItem("jwtToken", resp.data.token);
-          store.commit("setToken", resp.data.token);
-          // todo: resolve怎么用
-          // resolve(resp);
-        } else {
-          ElMessage.error('登录错误');
-        }
-      })
-      .then(() => {
-        // 获取用户信息
-        getUserInfo()
-            .then(resp => {
-              store.commit("setIsLogin", true);
-              store.commit("setUsername", resp.data.nickName);
-              store.commit("setRole",resp.data.type);
-              // store.commit("setUserPhoto", resp.data.userPhoto);
+    .then(resp => {
+      if (resp.msg === 'success') {
+        ElMessage({
+          message: '登录成功',
+          type: 'success',
+        })
+        localStorage.setItem("jwtToken", resp.data.token);
+        store.commit("setToken", resp.data.token);
+        // todo: resolve怎么用
+        // resolve(resp);
+      } else {
+        ElMessage.error('登录错误');
+      }
+    })
+    .then(() => {
+      // 获取用户信息
+      getUserInfo()
+        .then(resp => {
+          store.commit("setIsLogin", true);
+          store.commit("setUsername", resp.data.nickName);
+          store.commit("setRole", resp.data.type);
+          // store.commit("setUserPhoto", resp.data.userPhoto);
 
-              localStorage.setItem("role", resp.data.type);
-            })
-            .catch(resp => {
-              console.log(resp);
-              console.log("获取用户信息错误");
-            });
-      })
-      .then(() => {
-        // 有token返回就跳转到首页
-        router.push("/");
+          localStorage.setItem("role", resp.data.type);
+        })
+        .catch(resp => {
+          console.log(resp);
+          console.log("获取用户信息错误");
+        });
+    })
+    .then(() => {
+      // 有token返回就跳转到首页
+      router.push("/");
 
-        //记住密码
-        if (checkPassword.value) {
-          localStorage.setItem("username", loginForm.value.username);
-          localStorage.setItem("password", loginForm.value.password);
-        } else {
-          localStorage.removeItem("username");
-          localStorage.removeItem("password");
-        }
-      })
-      .catch(resp => {
-        console.log(resp);
-        ElMessage.error('登录异常');
-      })
+      //记住密码
+      if (checkPassword.value) {
+        localStorage.setItem("username", loginForm.value.username);
+        localStorage.setItem("password", loginForm.value.password);
+      } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+      }
+    })
+    .catch(resp => {
+      console.log(resp);
+      ElMessage.error('登录异常');
+    })
 }
 
 
@@ -159,9 +123,6 @@ onMounted(() => {
     loginForm.value.password = localStorage.getItem("password");
   }
 })
-
-
-
 </script>
 
 <style lang="scss" scoped>
@@ -173,6 +134,7 @@ $time-1: 60s;
 $time-2: 120s;
 $time-3: 180s;
 $time-4: 600s;
+
 * {
   margin: 0;
   padding: 0;
@@ -190,16 +152,24 @@ $time-4: 600s;
   /*   padding: 2em 0em; */
 }
 
-.container {
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  background-color: #fff;
-  padding: 40px 0px;
-  width: 240px;
-}
+// .container {
+//   align-items: center;
+//   display: flex;
+//   flex-direction: column;
+//   text-align: center;
+//   padding: 40px 0px;
+//   width: 100%;
+// }
+
+// .container {
+//   padding: 0px;
+//   margin: 0px;
+//   height: 100%;
+//   width: 100%;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// }
 
 .btn {
   letter-spacing: 0.1em;
@@ -212,7 +182,9 @@ $time-4: 600s;
   text-decoration: none;
   text-transform: uppercase;
   width: 100%;
+  margin-top: 10px;
 }
+
 .btn:hover {
   text-decoration: none;
 }
@@ -228,6 +200,7 @@ $time-4: 600s;
   transition: all 0.3s ease-in-out;
   border-radius: 10px;
 }
+
 .effect01:hover {
   border: 4px solid #666;
   background-color: #fff;
@@ -239,6 +212,7 @@ $time-4: 600s;
   transition: all 0.2s ease-out;
   z-index: 2;
 }
+
 .effect01:hover span {
   letter-spacing: 0.13em;
   color: #333;
@@ -257,9 +231,11 @@ $time-4: 600s;
   -webkit-transform: rotate(35deg);
   transform: rotate(35deg);
   width: 50px;
-  transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1); /*easeOutCirc*/
+  transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  /*easeOutCirc*/
   z-index: 1;
 }
+
 .effect01:hover:after {
   background: #fff;
   border: 20px solid #000;
@@ -285,18 +261,21 @@ input {
   height: 50px;
   width: 100%;
   border: none;
+
   &::placeholder {
     -webkit-transform: translateY(0px);
     transform: translateY(0px);
     -webkit-transition: 0.5s;
     transition: 0.5s;
   }
+
   &:hover,
   &:focus,
   &:active:focus {
     color: rgba(0, 0, 0, 0.64);
     outline: none;
     border-bottom: 1px solid rgba(0, 0, 0, 0.67);
+
     &::placeholder {
       color: rgba(0, 0, 0, 0.67);
       position: relative;
@@ -333,16 +312,18 @@ input {
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 95vh;
+  height: 100%;
+  /* Make the login area occupy 100% of the #login container */
 }
+
 .login-block {
   display: flex;
   flex-wrap: nowrap;
-  background-color: white;
+  background-color: #fff;
   align-items: flex-start;
   justify-content: space-between;
   width: 50vw;
-  height: 25vw;
+  height: 28vw;
   min-width: 600px;
   min-height: 320px;
   border-radius: 10px;
@@ -352,7 +333,7 @@ input {
   transition: transform 0.4s;
 }
 
-.login-block:hover{
+.login-block:hover {
 
   z-index: 999;
   -webkit-transition: all 400ms ease-in;
@@ -362,16 +343,7 @@ input {
 
 }
 
-.side-block {
-  display: flex;
-  align-items: center;
-  width: 70%;
-  height: 100%;
-  border-radius: 10px;
-  //background: url("https://joes-bucket.oss-cn-shanghai.aliyuncs.com/img/98C45201DA08BE5437F7FBFCAC1BEAFA.png")
-  //no-repeat;
-  background-size: cover;
-}
+
 
 .horizontal-flex-box {
   display: flex;
@@ -391,11 +363,14 @@ input {
 }
 
 .login-font {
-  font-size: 1.8em;
+  font-size: 2.5em;
+  margin-left: 2%;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  font-weight: bold;
 }
 
 .small-font {
-  font-size: 0.5em;
+  font-size: 1.1em;
   font-family: "FZGuDianK";
   color: #8f8f8f;
 }
@@ -410,12 +385,16 @@ input {
 .small-icon:hover {
   -webkit-transform: rotate(360deg);
 }
+
 /* Pauls awesome mixin */
 @function particles($max) {
   $val: 0px 0px $color-particle;
+
   @for $i from 1 through $max {
-    $val: #{$val}, random($spacing) + px random($spacing) + px $color-particle;
+    $val: #{$val},
+    random($spacing)+px random($spacing)+px $color-particle;
   }
+
   @return $val;
 }
 
@@ -440,23 +419,9 @@ body {
 }
 
 .page-bg {
-  background: $color-bg url("../../assets/login/LoginBG.jpg") no-repeat top
-  center;
+  background: $color-bg url("../../assets/login/BG.png") no-repeat top center;
   background-size: cover;
   z-index: -1;
-}
-
-/*虚化背景*/
-.bg-blur {
-  width: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  -webkit-filter: blur(5px);
-  -moz-filter: blur(5px);
-  -o-filter: blur(5px);
-  -ms-filter: blur(5px);
-  filter: blur(2px);
 }
 
 .particle,
@@ -526,6 +491,7 @@ body {
   from {
     transform: translateY(0px);
   }
+
   to {
     transform: translateY($spacing * -1);
   }
@@ -539,11 +505,33 @@ body {
 
 
 #login {
-  height: 88vh; /* Decrease the height to 90% of the viewport height */
+  height: 95vh;
 }
 
-.login-area {
-  height: 100%; /* Make the login area occupy 100% of the #login container */
-  /* Other styles for the login area */
+.left-block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+  border-radius: 10px;
+}
+
+.logo-image {
+  width: 80%;
+  height: 80%;
+  cursor: pointer;
+}
+
+.right-block {
+  width: 50%;
+  height: 100%;
+  border-radius: 10px;
+}
+
+.box {
+  padding-left: 25%;
+  width: 100%;
+  height: 100%;
 }
 </style>
