f2ef96e61c70d95782692485ae3606c2083684c3
うさぎ <momoyama.sawa@gmail.com>
2023-08-30 11:19:51 +0800

    fix: bug

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-  <NavBar />
-  <router-view />
+  <div id="app">
+    <div class="content">
+      <NavBar />
+      <router-view />
+    </div>
+    <div id="footer-container">
+      <MyFooter />
+    </div>
+  </div>
 </template>
 
-<style>
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
+}
+
+#footer-container {
+  flex-shrink: 0;
+}
+
 </style>
 
 <script setup>
 import NavBar from "@/components/common/NavBar.vue";
+import MyFooter from "@/components/common/MyFooter.vue";
 </script>
