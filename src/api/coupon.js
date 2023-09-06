b3dbc02a641e6d6346f53604333aa0c3c62a242d
Luluy233 <wayi_star@163.com>
2023-09-06 13:09:01 +0800

    个人中心：优惠券获取+删除前后端成功

--- src/api/coupon.js
+++ src/api/coupon.js
@@ -1,9 +1,9 @@
 import request from '@/utils/request';
 
-// 分页获取优惠券
+// 分页获取优惠券（买家）
 export function getCouponPage(data) {
     return request({
-        url:"https://mock.apifox.cn/m1/2970478-0-default/api/Payment/getCouponInPage/", //fronted
+        url:"/api/Payment/Coupon/getMyCoupon/", 
         method: "POST",
         headers: {
             Authorization: "Bearer " + localStorage.getItem("jwtToken")
@@ -12,11 +12,49 @@ export function getCouponPage(data) {
     })
 }
 
-// 删除用户某个优惠券
+// 分页获取优惠券（卖家）
+export function getStoreCoupon(data) {
+    return request({
+        url:"/api/Payment/Coupon/getStoreCoupon/", 
+        method: "POST",
+        data:data,
+    })
+}
 
+// 用户删除优惠券
 export function deleteUserCoupon(data) {
     return request({
-        url:"https://mock.apifox.cn/m1/2970478-0-default/api/Payment/deleteUserCoupon/", //fronted
+        url:"/api/Payment/Coupon/deleteByUser/",
+        method: "POST",
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("jwtToken")
+        },
+        data:data,
+    })
+}
+
+// 商家删除优惠券
+export function deleteStoreCoupon(data) {
+    return request({
+        url:"/api/Payment/Coupon/deleteByStore/",
+        method: "POST",
+        data:data,
+    })
+}
+
+// 商家生成优惠券
+export function generateCoupon(data) {
+    return request({
+        url:"/api/Payment/Coupon/generate/", 
+        method: "POST",
+        data:data,
+    })
+}
+
+// 商家为指定用户添加优惠券
+export function addCoupon(data) {
+    return request({
+        url:"/api/Payment/Coupon/add/", 
         method: "POST",
         headers: {
             Authorization: "Bearer " + localStorage.getItem("jwtToken")
--- src/components/Coupon/CouponCard.vue
+++ src/components/Coupon/CouponCard.vue
@@ -7,9 +7,9 @@
             <!-- v-if 折扣 -->
             <div  v-if="coupon.type === 'discount'">
             <div class="type-container">
-                <span :class="{'text-invalid': coupon.isValid === false}" class="discount-text" >{{ coupon.discount }}</span>折
+                <span :class="{'text-invalid': coupon.isValid === false}" class="discount-text" >{{ coupon.discount * 10 }}</span>折
             </div>
-            <div style="font-size: 35px;text-align: center;margin-top: 10px;font-weight: 300;">COUPON</div>
+            <div style="font-size: 30px;text-align: center;margin-top: 20px;font-weight: 300;">COUPON</div>
             </div>
 
             <!-- v-if 满减 -->
@@ -18,7 +18,7 @@
                 <span>满{{ coupon.bar }}减</span>
                 <span class="discount-text">{{ coupon.reduction }}</span>元
             </div>
-            <div style="font-size: 35px;text-align: center;margin-top: 10px;font-weight: 300; ">COUPON</div> 
+            <div style="font-size: 30px;text-align: center;margin-top: 20px;font-weight: 300; ">COUPON</div> 
             </div>
         </div>
 
@@ -27,10 +27,7 @@
             <!-- 使用限制 -->
             <div class="c-id" :class="{'text-invalid': coupon.isValid === false}">
             <div class="id-text">限用店铺：{{ coupon.storeId }}</div>
-            <div class="id-text">限用商品：{{ coupon.commodityId }}</div>
-            <!-- <div class="id-text">标签：{{ activeTag }}</div> -->
-            <!-- <div class="id-text">有效: {{ coupon.isValid}} </div> -->
-            <div style="text-align: center;font-size: 18px;font-weight: bold; margin:15px" >{{ coupon.couponId }}</div>
+            <div class="id-text" style="text-align: center;font-size: 20px;font-weight: bold; margin:20px 0" >{{ coupon.couponId }}</div>
             </div>
             <!-- 有效期 -->
             <div class="c-time" :class="{'text-invalid': coupon.isValid === false}">
@@ -66,8 +63,10 @@
     // 单张优惠券样式
   // 优惠券信息容器
   .c-container{
-    width:210px;
-    height: 400px;
+    height:100%;
+    width:15vw;
+    min-width:160px;
+    padding-bottom:10px;
     color:#e5c46e;
     background-color: #e5c46e;
     transition: all 0.3s ease;//过渡时间
@@ -91,20 +90,25 @@
   // 顶部优惠券类型
   .c-container .c-type{
     width: 100%;
-    height: 66%;
+    height: 70%;
+    min-height:160px;
     background-color: #161616;
+    padding:4vw 0 3vw 0;
     border-bottom: dashed #e5c46e;
   }
 
   .c-container .c-type .type-container{
+    height: 8vw;
     text-align: center; //文字居中
-    border-style:  double none;//边线
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-style: double none;
     border-width: 5px;
-    width:180px;
   }
 
   .c-container .c-type .discount-text{
-    font-size: 76px;
+    font-size: 5vw;
     font-weight: bold;
   }
 
@@ -112,14 +116,14 @@
   // 中部基本信息
   .c-container .c-info{
     color: #161616;
-    font-size: 10px;
+    font-size: 1vw;
     width: 100%;
     height:50%;
   }
 
   // 编号信息
   .c-container .c-info .c-id{
-    font-size: 14px;
+    font-size: 1em;
     margin: 5%;
     height: 40%;
     color:#000000ad;
@@ -133,12 +137,13 @@
 
   // 有效期
   .c-container .c-info .c-time{
-    font-size: 10px;
+    font-size: 3px;
     height: 15%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    margin: 5%;
     color:#0000008c;
+    overflow: hidden;
+    text-overflow: ellipsis; /* 将溢出的文本替换为省略号 */
+    // white-space: nowrap; /* 防止内容换行 */
   }
 
   // 根据优惠券是否有效改变颜色
