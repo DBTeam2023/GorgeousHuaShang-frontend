a4db9f122c4a607df5ece4215016209346bc1a1b
Jiayi Fan <105704921+Fan0110@users.noreply.github.com>
2023-09-08 21:54:50 +0800

    购物车功能全部完成

--- src/api/cart.js
+++ src/api/cart.js
@@ -14,7 +14,7 @@ export function getCartList() {
 // 删除购物车里的商品（支持批量删除）
 export function deleteCartGoods(data) {
     return request({
-        url: "https://mock.apifox.cn/m1/2970478-0-default/api/cart/deleteGood/",
+        url: "/api/Cart/deleteItems/",
         method: "POST",
         headers: {
             Authorization: "Bearer " + localStorage.getItem("jwtToken")
@@ -23,10 +23,22 @@ export function deleteCartGoods(data) {
     })
 }
  
+// 加入购物车的接口=>修改购物车商品数量
+export function addCartByGoods(data) {
+    return request({
+        url: "/api/Cart/addItem",
+        method: "POST",
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("jwtToken")
+        },
+        data: data,
+    });
+}
+
 // 购物车中下单结算————创建订单
 export function createOrder(data) {
     return request({
-        url: "http://47.115.231.142:1029/Order/createOrder",
+        url: "/api/Order/createOrder",
         method: "POST",
         headers: {
             Authorization: "Bearer " + localStorage.getItem("jwtToken")
